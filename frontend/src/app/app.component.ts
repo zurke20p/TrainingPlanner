e7aba8fec52e66a74d062f510c444eea48333c21
N1ktt <76476591+N1ktt@users.noreply.github.com>
2024-04-16 09:23:41 +0200

    Finished logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { ServerRequestService } from './services/server-request.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HttpClientModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  http = inject(ServerRequestService);
   async logout(){
-
+    const res = await this.http.logout();
+    console.log(res)
   }
 }
